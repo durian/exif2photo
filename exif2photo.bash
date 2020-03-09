62d25c3cc35a0703abed56e55eb48d4af4c24b9a
Peter Berck <durian@users.noreply.github.com>
2020-03-09 11:52:56 +0100

    Added font size calculations

--- exif2photo.bash
+++ exif2photo.bash
@@ -2,7 +2,8 @@
 #
 # Needs: exiv2, imagemagick
 #
-for FILE in ITM*JPG;
+EXPR="*.JPG"
+for FILE in ${EXPR};
 do
     echo "FILE: ${FILE}"
     PRE="${FILE%.JPG}"
@@ -27,6 +28,17 @@ do
     #
     JPG="${PRE}a.jpg"
     #echo $FILE $JPG
+    #
+    XxY=$(exiv2 ${FILE} | grep "Image size") #value=${str#*=}
+    XxY=${XxY#*:}
+    XxY="${XxY#"${XxY%%[![:space:]]*}"}" # remove trailing space
+    #echo $XxY
+    XYs=(${XxY//x/ })
+    #echo ${XYs[0]} ${XYs[1]}
+    Ys=${XYs[1]} # height
+    Fs=$((Ys / 40)) # font size 1/40 of height
+    #echo $Fs
+    #
     if [ ! -e "${JPG}" ]; then
 	echo convert "${FILE}" -auto-orient "${JPG}" # Rotate correctly
 	convert "${FILE}" -auto-orient "${JPG}"
@@ -37,23 +49,28 @@ do
 	if [ -e "./DOTMATRI.TTF" ]; then
 	    FONT="-font ./DOTMATRI.TTF"
 	fi
+	if [ -e "./ScreenMatrix.ttf" ]; then
+	    FONT="-font ./ScreenMatrix.ttf"
+	fi
 	#
 	if [ $MODE -eq 0 ]; then
 	    INFO0=" ${EXP_TIME} @ ${APERTURE} ISO ${ISO}"
 	    DRAW0=(-annotate +10+10 "${INFO0}")
 	    INFO1="${TS}"
 	    DRAW1=(-annotate +10+110 "${INFO1}")
-	    echo mogrify -pointsize 100 -fill yellow ${FONT} -gravity northwest "${DRAW0[@]}" -gravity northwest  "${DRAW1[@]}"  ${JPG}
-	    mogrify -pointsize 100 -fill yellow ${FONT} -gravity northwest "${DRAW0[@]}" -gravity northwest  "${DRAW1[@]}"  ${JPG}
+	    echo mogrify -pointsize ${Fs} -fill yellow ${FONT} -gravity northwest "${DRAW0[@]}" -gravity northwest  "${DRAW1[@]}"  ${JPG}
+	    mogrify -pointsize ${Fs} -fill yellow ${FONT} -gravity northwest "${DRAW0[@]}" -gravity northwest  "${DRAW1[@]}"  ${JPG}
 	fi
 	#
 	if [ $MODE -eq 1 ]; then
 	    INFO0=" ${EXP_TIME} @ ${APERTURE} ISO ${ISO} ${TS}"
-	    echo mogrify -pointsize 100 -fill orange "${FONT}" -gravity southeast "${DRAW0[@]}"  ${JPG}
+	    echo mogrify -pointsize ${Fs} -fill orange "${FONT}" -gravity southeast "${DRAW0[@]}"  ${JPG}
 	    DRAW0=(-annotate +4+4 "${INFO0}")
-	    mogrify -pointsize 100 -fill black  ${FONT} -gravity southeast "${DRAW0[@]}"  ${JPG}
-	    DRAW0=(-annotate +10+10 "${INFO0}")
-	    mogrify -pointsize 100 -fill orange ${FONT} -gravity southeast "${DRAW0[@]}"  ${JPG}
+	    mogrify -pointsize ${Fs} -fill black  ${FONT} -gravity southeast "${DRAW0[@]}"  ${JPG}
+	    OFF=$((Fs / 20)) # offset
+	    OFF=$((4 + OFF))
+	    DRAW0=(-annotate +${OFF}+${OFF} "${INFO0}") # +4+4 and +10+10 depends on fontsize...
+	    mogrify -pointsize ${Fs} -fill orange ${FONT} -gravity southeast "${DRAW0[@]}"  ${JPG}
 	fi
     else
 	echo "${EXP_TIME} @ ${APERTURE} ISO ${ISO}, ${TS}"
